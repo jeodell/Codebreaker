55c5ef9fe65070ce35e3044d612392a7a712c74d
Jason O'Dell <jason@vitalflohealth.com>
2023-04-07 17:10:43 -0400

    Adding checkboxes to words and sending the checked words to chatgpt asking for clues (TODO show response)

--- lib/image_detail.dart
+++ lib/image_detail.dart
@@ -1,8 +1,12 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:io';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:http/http.dart' as http;
 
 class DetailScreen extends StatefulWidget {
   const DetailScreen({super.key, required this.imagePath});
@@ -17,8 +21,8 @@ class DetailScreenState extends State<DetailScreen> {
   late final String _imagePath;
   late final TextRecognizer _textRecognizer;
   Size? _imageSize;
-  List<TextElement> _elements = <TextElement>[];
-  List<String>? _listCodenames;
+  final List<TextElement> _elements = <TextElement>[];
+  List<CodenamesWord>? _listWords;
 
   @override
   void initState() {
@@ -57,6 +61,7 @@ class DetailScreenState extends State<DetailScreen> {
 
   // To detect the words in an image
   Future<void> _recognizeWords() async {
+    await dotenv.load();
     _getImageSize(File(_imagePath));
 
     // Creating an InputImage object using the image path
@@ -68,24 +73,24 @@ class DetailScreenState extends State<DetailScreen> {
     const String pattern = r'^[a-zA-Z]+$';
     final RegExp regEx = RegExp(pattern);
 
-    final List<String> codenames = <String>[];
+    final List<CodenamesWord> codenames = <CodenamesWord>[];
 
     // Finding and storing the text String(s) and the TextElement(s)
     for (final TextBlock block in text.blocks) {
       for (final TextLine line in block.lines) {
-        print('line.text: ${line.text}');
+        debugPrint('line.text: ${line.text}');
         if (line.text.contains(' ')) {
           final List<String> words = line.text.split(' ');
           for (final String word in words) {
             if (regEx.hasMatch(word)) {
-              codenames.add(word);
+              codenames.add(CodenamesWord(word, false));
             }
           }
         } else {
           if (regEx.hasMatch(line.text)) {
-            codenames.add(line.text);
+            codenames.add(CodenamesWord(line.text, false));
             for (final TextElement element in line.elements) {
-              print('element: ${element}');
+              debugPrint('element: $element');
               _elements.add(element);
             }
           }
@@ -94,8 +99,46 @@ class DetailScreenState extends State<DetailScreen> {
     }
 
     setState(() {
-      _listCodenames = codenames;
+      _listWords = codenames;
     });
+  }
+
+  Future<void> _generateClues(List<String> words) async {
+    final Uri url = Uri.parse('https://api.openai.com/v1/completions');
+
+    final Map<String, String> headers = <String, String>{
+      'Content-Type': 'application/json;charset=UTF-8',
+      'Charset': 'utf-8',
+      'Authorization': 'Bearer ${dotenv.env['OPENAI_API_KEY']}'
+    };
+
+    String prompt = 'You are the spymaster for Codenames. Your words are ';
+    for (final String word in words) {
+      if (word == words.last) {
+        prompt += 'and $word. ';
+        break;
+      }
+      prompt += '$word, ';
+    }
+    prompt +=
+        '\n\nGenerate one word clues to link as many of your words together as possible.';
+    debugPrint(prompt);
+
+    final String data = jsonEncode(<String, dynamic>{
+      'model': 'text-davinci-003',
+      'prompt': prompt,
+      'temperature': 0.2,
+      'max_tokens': 64,
+    });
+
+    final http.Response response =
+        await http.post(url, headers: headers, body: data);
+    if (response.statusCode == 200) {
+      debugPrint(response.body);
+    } else {
+      debugPrint('Error: ${response.statusCode}');
+      debugPrint('Reason: ${response.reasonPhrase}');
+    }
   }
 
   @override
@@ -118,38 +161,60 @@ class DetailScreenState extends State<DetailScreen> {
                   alignment: Alignment.bottomCenter,
                   child: Card(
                     elevation: 8,
-                    color: Colors.white,
                     child: Padding(
-                      padding: const EdgeInsets.all(16.0),
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 16, horizontal: 4),
                       child: Column(
                         mainAxisSize: MainAxisSize.min,
-                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          const Padding(
-                            padding: EdgeInsets.only(bottom: 8.0),
-                            child: Text(
-                              'Identified Words',
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold,
+                          const Text(
+                            'Identified Words',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () {
+                              final List<String> words = <String>[];
+                              for (final CodenamesWord word in _listWords!) {
+                                if (word.selected) {
+                                  words.add(word.word);
+                                }
+                              }
+                              _generateClues(words);
+                            },
+                            child: const Text('Generate Clues'),
+                          ),
+                          if (_listWords != null)
+                            SizedBox(
+                              height: 200,
+                              child: SingleChildScrollView(
+                                child: Column(
+                                  children: _listWords!
+                                      .map((CodenamesWord e) =>
+                                          CheckboxListTile(
+                                            title: Text(e.word),
+                                            dense: true,
+                                            controlAffinity:
+                                                ListTileControlAffinity.leading,
+                                            onChanged: (bool? value) {
+                                              if (value != null) {
+                                                setState(() {
+                                                  e.selected
+                                                      ? e.selected = false
+                                                      : e.selected = true;
+                                                });
+                                              }
+                                            },
+                                            value: e.selected,
+                                          ))
+                                      .toList(),
+                                ),
                               ),
-                            ),
-                          ),
-                          SizedBox(
-                            height: MediaQuery.of(context).size.height * 0.4,
-                            child: SingleChildScrollView(
-                              child: _listCodenames != null
-                                  ? ListView.builder(
-                                      shrinkWrap: true,
-                                      physics: const BouncingScrollPhysics(),
-                                      itemCount: _listCodenames!.length,
-                                      itemBuilder:
-                                          (BuildContext context, int index) =>
-                                              Text(_listCodenames![index]),
-                                    )
-                                  : Container(),
-                            ),
-                          ),
+                            )
+                          else
+                            Container(),
                         ],
                       ),
                     ),
@@ -184,4 +249,23 @@ class DetailScreenState extends State<DetailScreen> {
             ),
     );
   }
+}
+
+class CodenamesWord {
+  CodenamesWord(this.word, this.selected);
+
+  final String word;
+  bool selected;
+}
+
+class GPTResponse {
+  GPTResponse(
+      this.id, this.object, this.created, this.model, this.choices, this.usage);
+
+  final String id;
+  final String object;
+  final String created;
+  final String model;
+  final String choices;
+  final String usage;
 }
